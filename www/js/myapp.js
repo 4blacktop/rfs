fbdce8dd42aa61baf6d38d90383e23a056327937
4blogga <4blogga@gmail.com>
2019-10-31 13:32:29 +1000

    1332

--- www/js/myapp.js
+++ www/js/myapp.js
@@ -1,18 +1,4 @@
-document.addEventListener("offline", onOffline, false);
-// document.addEventListener("online", onOnline, false);
-document.addEventListener("backbutton", onBackKeyDown, false);
-
-var timeoutID;
-
-function onBackKeyDown(e) {
-  e.preventDefault();
-}
-
 document.addEventListener('deviceready', function () {
-	// navigator.splashscreen.show();
 	alert('deviceready');
-	// navigator.splashscreen.hide();
 
-	spscreen();
-	// setTimeout(scan, 10000);
 });
