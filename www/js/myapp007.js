8a624db26acf17e790aba799a84a2864af41d2e7
4blogga <4blogga@gmail.com>
2019-11-03 14:53:06 +1000

    1453

--- www/js/myapp007.js
+++ www/js/myapp007.js
@@ -1,39 +1,3 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-		alert('deviceready');
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-
-
-
 function QRScan(){
     cordova.plugins.barcodeScanner.scan(
         function (result) {
@@ -61,11 +25,6 @@ function QRScan(){
             disableSuccessBeep: false // iOS and Android
         }
      );
-} 
-
-function scan(){
-	alert('scan');
-	// window.clearTimeout(timeoutID);
 }
 
 // document.addEventListener("offline", onOffline, false);
@@ -85,7 +44,9 @@ function scan(){
 // });
 /* 
 
-
+function scan(){
+	alert('scan');
+	// window.clearTimeout(timeoutID);
 	
 
 }	
