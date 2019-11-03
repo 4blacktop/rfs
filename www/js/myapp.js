8440224b1d11b60676ac033bd14845a9b275dfdb
4blogga <4blogga@gmail.com>
2019-11-03 14:38:53 +1000

    1438

--- www/js/myapp.js
+++ www/js/myapp.js
@@ -1,3 +1,32 @@
+function QRScan(){
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            alert("We got a barcode\n" +
+                  "Result: " + result.text + "\n" +
+                  "Format: " + result.format + "\n" +
+                  "Cancelled: " + result.cancelled);
+
+            document.getElementById('cryptoId').value = result.text
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        },
+        {
+            preferFrontCamera : true, // iOS and Android
+            showFlipCameraButton : true, // iOS and Android
+            showTorchButton : true, // iOS and Android
+            torchOn: true, // Android, launch with the torch switched on (if available)
+            saveHistory: true, // Android, save scan history (default false)
+            prompt : "Place a barcode inside the scan area", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations : true, // iOS
+            disableSuccessBeep: false // iOS and Android
+        }
+     );
+}
+
 // document.addEventListener("offline", onOffline, false);
 // document.addEventListener("online", onOnline, false);
 // document.addEventListener("backbutton", onBackKeyDown, false);
@@ -13,7 +42,7 @@
 
 	// setTimeout(scan, 10000);
 // });
-
+/* 
 
 function scan(){
 	alert('scan');
@@ -26,6 +55,7 @@ function scan(){
 
 
 function scan2(){
+	alert('scan2');
 	cordova.plugins.barcodeScanner.scan(
 	  function (result) {
 		  alert("We got a barcode\n" +
@@ -50,4 +80,4 @@ function scan2(){
 		  disableSuccessBeep: false // iOS and Android
 	  }
 	);
-}	
+}	 */
