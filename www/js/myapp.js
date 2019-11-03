3fa2e74477910c44cab71a0228127df8e2d27a21
4blogga <4blogga@gmail.com>
2019-11-03 20:48:34 +1000

    2048

--- www/js/myapp.js
+++ www/js/myapp.js
@@ -1,39 +1,44 @@
-/* var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-		// alert('deviceready');
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("offline", onOffline, false);
+document.addEventListener("online", onOnline, false);
+document.addEventListener("backbutton", onBackKeyDown, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function onOffline(){
+	document.getElementById('status-connection').style.backgroundColor = "#FB9898";
+	document.getElementById('status-traffic').style.backgroundColor = "#FB9898";
+}
 
-        console.log('Received Event: ' + id);
-    }
+function onOnline(){
+	document.getElementById('status-connection').style.backgroundColor = "#98FB98";
+	document.getElementById('status-traffic').style.backgroundColor = "#98FB98";
+}
 
-};
+function onBackKeyDown(e) {
+	e.preventDefault();
+}
+ 
+function test(){
+	// console.log(document.getElementById('result').innerHTML);
+	// document.getElementById('result').innerHTML = 'test';
+	result.text = "test";
+	
+	$.get( "http://moibiz27.ru/rfs/log.php?qrcode=" + result.text, function( data ) { // ajax GET // console.log('data: ' + data.toSource());
+	if(data == Boolean(false)) { // error qr code not found
+		// document.getElementById('status-connection').style.backgroundColor = "#9898FB";
+		// document.getElementById('status-traffic').style.backgroundColor = "#9898FB";
+	} else {
+		// console.log(data);
+		// document.getElementById('status-traffic').innerHTML = result.text;
+		// if (data['date-first-query'] == null) { // first query
+			// ok(data);
+		// } else { // already queried
+			// warning(data);
+		}
+	});
+	
+	
 
-
- */
+	
+}
 
 function scan(){
 	document.getElementById('result').value = 'test';
@@ -41,7 +46,19 @@ function scan(){
 		function(result){
 			//success callback
 			// alert(JSON.stringify(result));
-			document.getElementById('result').value = result.textж
+			document.getElementById('result').innerHTML = result.text;
+			$.get( "http://moibiz27.ru/rfs/log.php?qrcode=" + result.text, function( data ) { // ajax GET // console.log('data: ' + data.toSource());
+				if(data == Boolean(false)) { // error qr code not found
+					document.getElementById('status-connection').style.backgroundColor = "#9898FB";
+					document.getElementById('status-traffic').style.backgroundColor = "#9898FB";
+				} else {
+					// if (data['date-first-query'] == null) { // first query
+						// ok(data);
+					// } else { // already queried
+						// warning(data);
+					// }
+				}
+			}); 
 		},
 	function(error){
 			//error callback
@@ -183,3 +200,42 @@ function scan2(){
 	  }
 	);
 }	 */
+
+
+
+/* var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+		// alert('deviceready');
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+
+};
+
+
+ */
---
+++ www/php/log.php
@@ -0,0 +1,10 @@
+<?php
+// header("Content-Type: text/html; charset=utf-8"; "Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
+error_reporting(E_ALL);
+$person = "\r\n" . $_GET["qrcode"];
+echo $person;
+$file = 'log.txt';
+file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
+
+?>
